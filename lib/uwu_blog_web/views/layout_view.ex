a88182df051b86b57a89e824c743f3545b4a613f
Cocoa <i@uwucocoa.moe>
2023-01-05 01:34:47 +0000

    support embedded HEEx templates in markdown files

--- lib/uwu_blog_web/views/layout_view.ex
+++ lib/uwu_blog_web/views/layout_view.ex
@@ -4,4 +4,12 @@ defmodule UwUBlogWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def site_title do
+    UwUBlog.site_title
+  end
+
+  def nav_items do
+    UwUBlog.nav_items
+  end
 end
--- lib/uwu_blog_web/views/page_view.ex
+++ lib/uwu_blog_web/views/page_view.ex
@@ -1,3 +1,10 @@
 defmodule UwUBlogWeb.PageView do
   use UwUBlogWeb, :view
+
+  def render("embed.html", %{post: post}=assigns) do
+    body = UwUBlog.HTMLEngine.compile(post.frontmatter["template"]["heex"], [])
+    {rendered, _} = Code.eval_quoted(body, [assigns: assigns])
+    IO.inspect(rendered)
+    rendered
+  end
 end
