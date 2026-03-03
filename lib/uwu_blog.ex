71fef3221ad305d9a6cbcd7392366d8618d24bc8
Cocoa <i@uwucocoa.moe>
2026-03-04 07:43:36 +0900

    updated blog
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- lib/uwu_blog.ex
+++ lib/uwu_blog.ex
@@ -21,29 +21,4 @@ defmodule UwUBlog do
       }
     ]
   end
-
-  def kira do
-    [
-      %{
-        name: "桃桃乌龙⸝⸝⸝♡",
-        link: "https://peachoolong-uwu.github.io"
-      },
-      %{
-        name: "<sup>^</sup>Makito<sup>^</sup>",
-        link: "https://maki.to"
-      },
-      %{
-        name: "千千",
-        link: "https://wwyqianqian.github.io/"
-      },
-      %{
-        name: "千夏",
-        link: "https://blog.uuz.moe/"
-      },
-      %{
-        name: "雪羽",
-        link: "https://yukiha.live/"
-      }
-    ]
-  end
 end
